01f6c45cafb63509184988c1ec0aaa4ce227498d
mpab <mick_bythell@hotmail.com>
2017-08-22 10:07:38 +0200

    implemented drone connection & control

--- PubNubDroneController/controller.js
+++ PubNubDroneController/controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var PubNub = require('pubnub');
+const PubNub = require('pubnub');
  
 var pubnub = new PubNub({
     subscribeKey: "sub-c-4e681cea-8380-11e7-8979-5e3a640e5579",
@@ -8,9 +8,76 @@ var pubnub = new PubNub({
     ssl: true
 });
 
+const Drone = require("rolling-spider");
+var drone = new Drone();
+
+const STEPS = 10;
+
+drone.connect(function () {
+    drone.setup(function () {
+        drone.flatTrim();
+        drone.startPing();
+        drone.flatTrim();
+        console.log('Connected to: ', drone.name);
+    })
+})
+
 pubnub.addListener({
-    message: function(message){
-        console.log(message.message.command)
+    message: function(message) {
+        switch (message.message.command) {
+            case "setup":
+                console.log('drone:', message.message.command);
+                drone.setup(function () {
+                    drone.flatTrim();
+                    drone.startPing();
+                    drone.flatTrim();
+                })
+            break;
+
+            case "takeoff":
+                console.log('drone:', message.message.command);
+                drone.takeOff(); 
+                break;
+
+            case "land":
+                console.log('drone:', message.message.command);
+                drone.land();
+                break;
+
+            case "shutdown":
+                console.log('drone:', message.message.command);
+                drone.land();
+                process.exit();
+                break;
+
+            case "forward":
+                console.log('drone:', message.message.command);
+                drone.forward({ steps: STEPS });
+                break;
+
+            case "backward":
+                console.log('drone:', message.message.command);
+                drone.backward({ steps: STEPS });
+                break;
+
+            case "left":
+                console.log('drone:', message.message.command);
+                drone.tiltLeft({ steps: STEPS });
+                break;
+
+            case "right":
+                console.log('drone:', message.message.command);
+                drone.tiltRight({ steps: STEPS });
+                break;
+
+            case undefined:
+                console.log('undefined drone command:', message.message.command);
+                break;
+            
+            default:
+                console.log('unrecognised drone command:', message.message.command);
+                break;
+        }
     }
 });
 
